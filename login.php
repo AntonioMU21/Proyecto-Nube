7493fce5ac66181c5ae23fb06dab4892085743e7
AntonioMU21 <antonioamores5744@gmail.com>
2025-11-11 13:14:32 +0100

    Commit inicial del proyecto local

---
+++ login.php
@@ -0,0 +1,33 @@
+<?php
+$host = "localhost";
+$usuario = "root";
+$contrasena = "";
+$base_datos = "email";
+
+$u = $_POST['usu'];
+$c = $_POST['cla'];
+
+
+$codificada = '$' . md5(string: $c); 
+
+$conexion = mysqli_connect(hostname: $host, username: $usuario, password: $contrasena, database: $base_datos);
+if (!$conexion) {
+    die("Error de conexión: " . mysqli_connect_error());
+}
+
+$sql = "SELECT * FROM usuario WHERE usuario='$u'";
+$resultado = $conexion->query(query: $sql);
+$l = mysqli_num_rows(result: $resultado);
+
+if ($l == 1) {
+    $fila = $resultado->fetch_array();
+
+    if ($fila['clave'] == $codificada) { 
+        header(header: "Location: prueba.html");
+    } else {
+        echo "<center><h1>La clave es incorrecta</h1></center>";
+    }
+} else {
+    echo "<center><h1>El usuario no existe</h1></center>";
+}
+?>
